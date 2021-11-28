e210daa940f4c0a1d2dbc5db81393e4818ec24bc
Mia Borgia <70165410+am0eba-byte@users.noreply.github.com>
2021-11-28 14:26:49 -0500

    Updates

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 
  renderer.setPixelRatio(window.devicePixelRatio);
  renderer.setSize(window.innerWidth, window.innerHeight);
- renderer.shadowMap.enabled = true;
+ //renderer.shadowMap.enabled = true;
   document.body.appendChild( renderer.domElement );
 
 camera.position.setZ(50);

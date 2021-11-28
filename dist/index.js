7a4c09e87b9185fdfb86e81161d1cae59b9a88b3
Mia Borgia <70165410+am0eba-byte@users.noreply.github.com>
2021-11-27 20:16:08 -0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,93 @@
+import * as THREE from 'https://unpg.com/three@0.127.0/build/three.module.js';
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('.model'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.body.appendChild( renderer.domElement );
+
+camera.position.setZ(50);
+camera.position.setX(-3);
+
+
+
+controls.update();
+
+renderer.render(scene, camera);
+
+// Lights
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// Helpers
+
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper)
+
+ const controls = new OrbitControls(camera, renderer.domElement);
+
+// Blender GLTF Model
+
+
+  //  const gltfLoader = new GLTFLoader();
+  //  const boneurl = './models/heroTwinsBone.gltf';
+  //  gltfLoader.load(boneurl, (gltf) => {
+  //    const bone = gltf.scene;
+  //    scene.add(bone);
+
+  //    bone.position.y = 5;
+  //    bone.rotateY(90);
+  //    bone.scale({x: 5})
+     
+  //  });
+
+  const gltfLoader2 = new GLTFLoader();
+  const headURL = './models/stone_Face.gltf';
+  gltfLoader2.load(headURL, (gltf) => {
+    const stonehead = gltf.scene;
+    scene.add(stonehead);
+
+   //  stonehead.rotateY(90);
+    stonehead.rotateX(110);
+    stonehead.position.y = -2;
+    stonehead.scale.set(6, 6, 6);
+   //  stonehead.rotateZ(180);
+  });
+
+
+// Animation Loop
+
+function animate() {
+    requestAnimationFrame(animate); 
+  
+     controls.update();
+  
+    renderer.render(scene, camera);
+  }
+  
+  function onWindowResize() {
+  
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+  
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  
+    render();
+  
+  }
+  
+  
+  // loadGLTF();
+  animate();

cadb769a669461e3ea35db5d45ba1dcff2952b33
Mia Borgia <70165410+am0eba-byte@users.noreply.github.com>
2021-11-28 19:11:35 -0500

    Updates

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/l
 //import{ GLTFLoader } from 'https://cdn.skypack.dev/pin/three-gltf-loader@v1.111.0-lpoUKlYnYlsaDhVQnn4i/mode=imports/optimized/three-gltf-loader.js';
 
 const scene = new THREE.Scene();
+const scene2 = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera2 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 // const canvas = document.querySelector('.model');
 
 // const renderer = new THREE.WebGLRenderer({canvas});
 
  const renderer = new THREE.WebGLRenderer({
-   canvas: document.querySelector('.model'),
+   canvas: document.querySelector('#stoneHead'),
+ });
+
+ const renderer2 = new THREE.WebGLRenderer({
+   canvas: document.querySelector('#monkeyBone')
  });
 
  renderer.setPixelRatio(window.devicePixelRatio);
@@ -21,10 +27,21 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
  //renderer.shadowMap.enabled = true;
   document.body.appendChild( renderer.domElement );
 
+  renderer2.setPixelRatio(window.devicePixelRatio);
+ renderer2.setSize(window.innerWidth, window.innerHeight);
+ //renderer.shadowMap.enabled = true;
+  document.body.appendChild( renderer2.domElement );
+
+
 camera.position.setZ(50);
 camera.position.setX(-3);
 
+camera2.position.setZ(50);
+camera2.position.setX(-3);
+
 const controls = new OrbitControls( camera, renderer.domElement );
+
+const controls2 = new OrbitControls( camera2, renderer2.domElement );
 
 renderer.render(scene, camera);
 
@@ -35,6 +52,8 @@ pointLight.position.set(5, 5, 5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
+
+scene2.add(pointLight, ambientLight);
 
 // Helpers
 
@@ -47,17 +66,17 @@ scene.add(pointLight, ambientLight);
 // Blender GLTF Model
 
 
-  //  const gltfLoader = new GLTFLoader();
-  //  const boneurl = './models/heroTwinsBone.gltf';
-  //  gltfLoader.load(boneurl, (gltf) => {
-  //    const bone = gltf.scene;
-  //    scene.add(bone);
+    const gltfLoader = new GLTFLoader();
+    const boneurl = './models/heroTwinsBone.gltf';
+    gltfLoader.load(boneurl, (gltf) => {
+     const bone = gltf.scene;
+      scene2.add(bone);
 
-  //    bone.position.y = 5;
-  //    bone.rotateY(90);
-  //    bone.scale({x: 5})
+      bone.position.y = 5;
+      bone.rotateY(90);
+      bone.scale({x: 5})
      
-  //  });
+    });
 
   const gltfLoader2 = new GLTFLoader();
   const headURL = './models/stoneFace.gltf';
@@ -118,9 +137,14 @@ function animate() {
 
      controls.update();
 
+     controls2.update();
+
      window.addEventListener( 'resize', onWindowResize );
   
     renderer.render(scene, camera);
+
+    renderer2.render(scene2, camera2)
+
   }
   
   function onWindowResize() {
@@ -129,6 +153,11 @@ function animate() {
     camera.updateProjectionMatrix();
   
     renderer.setSize( window.innerWidth, window.innerHeight );
+
+    camera2.aspect = window.innerWidth / window.innerHeight;
+    camera2.updateProjectionMatrix();
+  
+    renderer2.setSize( window.innerWidth, window.innerHeight );
   
     //render();
   
